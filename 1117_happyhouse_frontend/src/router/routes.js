0c408ef814e11e87633a009bc4b86ef50fc9b3f0
BaeJiHwan <bgh1906@naver.com>
2021-11-23 02:31:01 +0900

    프론트엔드 로그인기능 좌측으로 변경

--- 1117_happyhouse_frontend/src/router/routes.js
+++ 1117_happyhouse_frontend/src/router/routes.js
@@ -44,6 +44,7 @@ const routes = [
       {
         path: "/profile",
         name: "profile",
+        beforeEnter: onlyAuthUser,
         component: () => import("../views/Pages/UserProfile.vue"),
       },
       {
--- 1117_happyhouse_frontend/src/views/Pages/Login.vue
+++ 1117_happyhouse_frontend/src/views/Pages/Login.vue
@@ -7,16 +7,27 @@
           <b-row class="justify-content-center">
             <b-col xl="5" lg="6" md="8" class="px-5">
               <h1 class="text-white">Welcome!</h1>
-              <p class="text-lead text-white">Use these awesome forms to login or create new account in your project for
-                free.</p>
+              <p class="text-lead text-white">
+                Use these awesome forms to login or create new account in your
+                project for free.
+              </p>
             </b-col>
           </b-row>
         </div>
       </b-container>
       <div class="separator separator-bottom separator-skew zindex-100">
-        <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1"
-             xmlns="http://www.w3.org/2000/svg">
-          <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
+        <svg
+          x="0"
+          y="0"
+          viewBox="0 0 2560 100"
+          preserveAspectRatio="none"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <polygon
+            class="fill-default"
+            points="2560 0 2560 100 0 100"
+          ></polygon>
         </svg>
       </div>
     </div>
@@ -25,47 +36,41 @@
       <b-row class="justify-content-center">
         <b-col lg="5" md="7">
           <b-card no-body class="bg-secondary border-0 mb-0">
-            <b-card-header class="bg-transparent pb-5"  >
-              <div class="text-muted text-center mt-2 mb-3"><small>Sign in with</small></div>
-              <div class="btn-wrapper text-center">
-                <a href="#" class="btn btn-neutral btn-icon">
-                  <span class="btn-inner--icon"><img src="img/icons/common/github.svg"></span>
-                  <span class="btn-inner--text">Github</span>
-                </a>
-                <a href="#" class="btn btn-neutral btn-icon">
-                  <span class="btn-inner--icon"><img src="img/icons/common/google.svg"></span>
-                  <span class="btn-inner--text">Google</span>
-                </a>
-              </div>
-            </b-card-header>
             <b-card-body class="px-lg-5 py-lg-5">
               <div class="text-center text-muted mb-4">
-                <small>Or sign in with credentials</small>
+                <h2>Login</h2>
               </div>
-              <validation-observer v-slot="{handleSubmit}" ref="formValidator">
+              <validation-observer
+                v-slot="{ handleSubmit }"
+                ref="formValidator"
+              >
                 <b-form role="form" @submit.prevent="handleSubmit(onSubmit)">
-                  <base-input alternative
-                              class="mb-3"
-                              name="Email"
-                              :rules="{required: true, email: true}"
-                              prepend-icon="ni ni-email-83"
-                              placeholder="Email"
-                              v-model="model.email">
+                  <base-input
+                    id="userid"
+                    v-model="user.userid"
+                    required
+                    placeholder="ID"
+                    @keyup.enter="confirm"
+                  >
                   </base-input>
 
-                  <base-input alternative
-                              class="mb-3"
-                              name="Password"
-                              :rules="{required: true, min: 6}"
-                              prepend-icon="ni ni-lock-circle-open"
-                              type="password"
-                              placeholder="Password"
-                              v-model="model.password">
+                  <base-input
+                    type="password"
+                    id="userpwd"
+                    v-model="user.userpwd"
+                    required
+                    placeholder="PASSWORD"
+                    @keyup.enter="confirm"
+                  >
                   </base-input>
-
-                  <b-form-checkbox v-model="model.rememberMe">Remember me</b-form-checkbox>
                   <div class="text-center">
-                    <base-button type="primary" native-type="submit" class="my-4">Sign in</base-button>
+                    <base-button
+                      type="primary"
+                      variant="primary"
+                      class="my-4"
+                      @click="confirm"
+                      >Sign in</base-button
+                    >
                   </div>
                 </b-form>
               </validation-observer>
@@ -73,10 +78,14 @@
           </b-card>
           <b-row class="mt-3">
             <b-col cols="6">
-              <router-link to="/dashboard" class="text-light"><small>Forgot password?</small></router-link>
+              <router-link to="/dashboard" class="text-light"
+                ><small>Forgot password?</small></router-link
+              >
             </b-col>
             <b-col cols="6" class="text-right">
-              <router-link to="/register" class="text-light"><small>Create new account</small></router-link>
+              <router-link to="/register" class="text-light"
+                ><small>Create new account</small></router-link
+              >
             </b-col>
           </b-row>
         </b-col>
@@ -85,25 +94,41 @@
   </div>
 </template>
 <script>
-  export default {
-    data() {
-      return {
-        model: {
-          email: '',
-          password: '',
-          rememberMe: false
-        }
-      };
-    },
-    methods: {
-      onSubmit() {
-        // this will be called only after form is valid. You can do api call here to login
+import { mapState, mapActions } from "vuex";
+
+const memberStore = "memberStore";
+
+export default {
+  name: "MemberLogin",
+  data() {
+    return {
+      user: {
+        userid: null,
+        userpwd: null,
+      },
+    };
+  },
+  computed: {
+    ...mapState(memberStore, ["isLogin", "isLoginError"]),
+  },
+  methods: {
+    ...mapActions(memberStore, ["userConfirm", "getUserInfo"]),
+    async confirm() {
+      await this.userConfirm(this.user);
+      let token = sessionStorage.getItem("access-token");
+      if (this.isLogin) {
+        await this.getUserInfo(token);
+        this.$router.push({ name: "dashboard" });
       }
-    }
-  };
+    },
+    movePage() {
+      this.$router.push({ name: "SignUp" });
+    },
+  },
+};
 </script>
 <style>
 .bg-gradient-success {
-  background: #2186C4 !important;
+  background: #2186c4 !important;
 }
 </style>
