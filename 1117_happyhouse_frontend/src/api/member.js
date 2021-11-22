218e7d59ff97c0a454ead8a4272a2e3ac4057fe5
BaeJiHwan <bgh1906@naver.com>
2021-11-22 22:36:27 +0900

    프론트엔드 member api 추가

--- 1117_happyhouse_frontend/src/api/member.js
+++ 1117_happyhouse_frontend/src/api/member.js
@@ -11,6 +11,18 @@ async function findById(userid, success, fail) {
   await api.get(`/user/info/${userid}`).then(success).catch(fail);
 }
 
+async function deleteMember(userid, success, fail) {
+  await api.delete(`/user/${userid}`).then(success).catch(fail);
+}
+
+async function registerMember(user, success, fail) {
+  await api.post(`/user`, JSON.stringify(user)).then(success).catch(fail);
+}
+
+async function updateMember(user, success, fail) {
+  await api.put(`/user`, JSON.stringify(user)).then(success).catch(fail);
+}
+
 // function logout(success, fail)
 
-export { login, findById };
+export { login, findById, deleteMember, registerMember, updateMember };
